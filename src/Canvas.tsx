a9506f7f898473b6956e18bd5475aa34f217e79c
Joxter <joxterrr@gmail.com>
2025-07-13 19:49:56 +0200

    simple canvas

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,5 +1,93 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+
+const scale = window.devicePixelRatio;
+
+const width = 800;
+const height = 400;
+
+let history: any[] = [];
 
 export function Canvas() {
-  return <div>canvas</div>;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    canvas.width = Math.floor(width * scale);
+    canvas.height = Math.floor(height * scale);
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    ctx.scale(scale, scale);
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 2;
+    ctx.lineCap = "round";
+  }, []);
+
+  const startDrawing = (e: React.MouseEvent) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+    history.push(["start", x, y]);
+    setIsDrawing(true);
+  };
+
+  const draw = (e: React.MouseEvent) => {
+    if (!isDrawing) return;
+
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    const rect = canvas.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    ctx.lineTo(x, y);
+    history.push(["move", x, y]);
+    ctx.stroke();
+  };
+
+  const stopDrawing = () => {
+    if (history.at(-1)[0] !== "end") {
+      history.push(["end"]);
+    }
+    console.log(history);
+
+    setIsDrawing(false);
+  };
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={width}
+      height={height}
+      style={{
+        border: "1px solid #ccc",
+        cursor: "crosshair",
+        width: width + "px",
+        height: height + "px",
+      }}
+      onMouseDown={startDrawing}
+      onMouseMove={draw}
+      onMouseUp={stopDrawing}
+      onMouseLeave={stopDrawing}
+    />
+  );
 }
