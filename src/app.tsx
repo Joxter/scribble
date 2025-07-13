14527309665d1d5e5a63692c3c37d28146087a04
Joxter <joxterrr@gmail.com>
2025-07-13 20:59:31 +0200

    Updates

--- src/app.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./main.css";
-import { createRoot } from "react-dom/client";
-import { Router } from "./Router";
-
-const root = createRoot(document.getElementById("app")!);
-root.render(<Router />);
