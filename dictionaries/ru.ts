526bda240f581904a0c61e76d9f3622d2bb43fa2
Joxter <joxterrr@gmail.com>
2025-08-16 20:17:30 +0200

    add 1k Claude words

--- dictionaries/ru.ts
+++ dictionaries/ru.ts
@@ -1,4 +1,5 @@
 import { alisaWords } from "./ru-alisa";
 import { words } from "./ru-300-chatgpt";
+import { words as words1000 } from "./ru-1000-claude";
 
-export const ru = [...alisaWords, ...words];
+export const ru = [...alisaWords, ...words, ...words1000];
